0f2a45be40a4edb05f6a77fe182411953068b1af
ryanlazo <ryanlazo12@gmail.com>
2018-01-23 10:50:46 -0500

    formatted output

--- src/edu/cnm/deepdive/Abacus.java
+++ src/edu/cnm/deepdive/Abacus.java
@@ -1,5 +1,7 @@
 package edu.cnm.deepdive;
 
+import java.util.Arrays;
+
 public class Abacus {
 
   public static void main(String[] args) {
@@ -7,7 +9,11 @@ public class Abacus {
     for (String s : args) {
       sum += Double.parseDouble(s);
     }
-    System.out.println(sum);
+    //System.out.print("sum = ");
+    //System.out.println(sum);
+    System.out.printf("Sum of %s = %5.2f%n", Arrays.toString(args), sum);
+    
+
   }
 }
 
